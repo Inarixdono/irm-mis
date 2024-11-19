7941a0eadd81d0e3f687964dedb4c7288fc0252a
IRM IT <itramosmotors@gmail.com>
2024-11-19 17:26:51 -0400

    add configuration and database initialization modules

---
+++ core/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config:
+    DATABASE_URI = os.getenv("DATABASE_URI")
+    
+settings = Config()
---
+++ core/database.py
@@ -0,0 +1,18 @@
+from fastapi import Depends, FastAPI
+from contextlib import asynccontextmanager
+from typing import Annotated
+from core.config import settings
+from sqlmodel import Session, create_engine, SQLModel
+
+engine = create_engine(settings.DATABASE_URI)
+
+@asynccontextmanager
+async def init_db(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+    
+async def get_session():
+    with Session(engine) as session:
+        yield session
+        
+SessionDependency = Annotated[Session, Depends(get_session)]
