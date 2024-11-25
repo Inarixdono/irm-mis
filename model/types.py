507b8e7e13dd9c3697a936e0028f14f1e5f03b28
IRM IT <itramosmotors@gmail.com>
2024-11-25 18:12:24 -0400

    fix: update zip_code field in Address model to enforce min_length constraint

--- model/types.py
+++ model/types.py
@@ -1,11 +1,11 @@
 from sqlmodel import SQLModel, Field
-from datetime import date, datetime
+from datetime import datetime
 
 class Address(SQLModel):
     street: str | None = Field(default=None, max_length=100)
     city: str | None = Field(default=None, max_length=100)
     state: str | None = Field(default=None, max_length=100)
-    zip_code: str | None = Field(default=None, max_length=5)
+    zip_code: str | None = Field(default=None, min_length=5, max_length=5)
     country: str | None = Field(default=None, max_length=100)
     reference: str | None = Field(default=None, max_length=100)
     
