98cfc1b1ae27000ccfcdb4b494f0d0256fdd745b
IRM IT <itramosmotors@gmail.com>
2024-11-25 12:46:41 -0400

    refactor: rename Generic class to CRUD and update method parameters for clarity

--- service/crud.py
+++ service/crud.py
@@ -1,11 +1,11 @@
 from core.database import SessionDependency, SQLModel
 
-class Generic:
+class CRUD:
     def __init__(self, session: SessionDependency):
         self.session = session
         
-    def create(self, BaseModel: SQLModel, model_in: SQLModel):
-        resource = BaseModel(**model_in.model_dump())
+    def create(self, base_model: SQLModel, model_in: SQLModel):
+        resource = base_model(**model_in.model_dump())
         self.session.add(resource)
         self.session.commit()
         self.session.refresh(resource)
