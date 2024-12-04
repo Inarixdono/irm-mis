be34d52875f69ef180305470a55d4c9c0b1103a2
IRM IT <itramosmotors@gmail.com>
2024-12-04 17:57:17 -0400

    feat: update Person controller to use new model types for create endpoint

--- service/person/controller.py
+++ service/person/controller.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from service.person.model import PersonIn, PersonOut, Person
+from service.person.model import PersonCreate, PersonPublic, Person
 from service.person.service import Person as PersonService
 from fastapi import APIRouter, Depends
 
@@ -9,6 +9,6 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )   
         
-@router.post("/", response_model=PersonOut)
-async def create_person(person: PersonIn, service: Annotated[PersonService, Depends()]):
+@router.post("/", response_model=PersonPublic)
+async def create_person(person: PersonCreate, service: Annotated[PersonService, Depends()]):
     return service.create(Person, person)
