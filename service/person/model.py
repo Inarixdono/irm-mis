4950113c600af9402cfa2b8620ef47a99a104df4
IRM IT <itramosmotors@gmail.com>
2024-12-04 18:44:09 -0400

    feat: update PersonBase model to use DocumentType for document_type field

--- service/person/model.py
+++ service/person/model.py
@@ -1,11 +1,11 @@
 from sqlmodel import SQLModel, Field
-from core.types import Address, Audit, Model
+from core.types import Address, Audit, Model, DocumentType
 
 
 class PersonBase(SQLModel):
     name: str = Field(max_length=100)
-    document_type: str | None = None
-    document_number: str | None = Field(default=None, max_length=20, unique=True)
+    document_type: DocumentType
+    document_number: str = Field(max_length=20, unique=True)
     phone_number: str | None = Field(default=None, max_length=10)
 
 
