4c3e0b277863415beaf03286293920e8df44ae5b
IRM IT <itramosmotors@gmail.com>
2024-12-04 17:57:08 -0400

    feat: refactor Person model classes to use custom Model type and improve structure

--- service/person/model.py
+++ service/person/model.py
@@ -1,23 +1,21 @@
 from datetime import date
 from sqlmodel import SQLModel, Field
-from core.types import Address, Audit
+from core.types import Address, Audit, Model
 
 class PersonBase(SQLModel):
     name: str = Field(max_length=100)
-    surname: str | None = Field(default=None, max_length=100)
-    birthdate: date | None = None
-    gender: str | None = Field(default=None, max_length=1)
     document_type: str | None = None
     document_number: str | None = Field(default=None, max_length=20, unique=True)
     phone_number: str | None = Field(default=None, max_length=10)
 
-class PersonIn(Audit, Address, PersonBase, SQLModel):
+class PersonCreate(Address, PersonBase):
     pass
 
+class PersonUpdate(PersonCreate, Model):
+    pass
 
-class Person(PersonIn, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class PersonPublic(PersonBase, Model):
+    pass
 
-class PersonOut(SQLModel):
-    id: int
-    name: str
+class Person(Audit, PersonCreate, Model, table=True):
+    pass
