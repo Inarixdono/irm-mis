a177c8f616a9bd8bbf999fbd243e7bc0b7528447
IRM IT <itramosmotors@gmail.com>
2024-12-06 10:57:02 -0400

    feat: enhance CRUD operations with HTTPException handling for resource not found

--- core/crud.py
+++ core/crud.py
@@ -1,5 +1,6 @@
 from core.database import SessionDependency, SQLModel
 from core.types import Model
+from fastapi import HTTPException
 from sqlmodel import select
 
 
@@ -8,23 +9,27 @@ class CRUD:
         self.session = session
 
     def read(self, base_model: Model, id: int):
-        statement = select(base_model).where(base_model.id == id)
-        return self.session.exec(statement).first()
+        resource = self.session.get(base_model, id)
+        if not resource:
+            raise HTTPException(status_code=404, detail="Resource not found")
+        return resource
 
-    def read_all(self, base_model: Model):
+    def read_all(self, base_model: SQLModel):
         statement = select(base_model)
         return self.session.exec(statement).all()
 
-    def create(self, base_model: Model, model_in: SQLModel):
-        resource = base_model(**model_in.model_dump())
+    def create(self, base_model: SQLModel, model_create: SQLModel):
+        resource = base_model.model_validate(model_create)
         self.session.add(resource)
         self.session.commit()
         self.session.refresh(resource)
         return resource
 
-    def update(self, base_model: Model, model_in: Model):
-        resource = self.session.get(base_model, model_in.id)
-        resource_data = model_in.model_dump(exclude_unset=True)
+    def update(self, base_model: SQLModel, model_update: Model):
+        resource = self.session.get(base_model, model_update.id)
+        if not resource:
+            raise HTTPException(status_code=404, detail="Resource not found")
+        resource_data = model_update.model_dump(exclude_unset=True)
         resource.sqlmodel_update(resource_data)
         self.session.add(resource)
         self.session.commit()
