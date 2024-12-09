ea7c495583b071867ee57ec6f085d17d868fa0e7
IRM IT <itramosmotors@gmail.com>
2024-12-09 12:28:45 -0400

    feat: implement CRUD endpoints for User model in controller

--- service/user/controller.py
+++ service/user/controller.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from .model import UserCreate, UserUpdate, UserPublic, User
+from .service import User as UserService
+from typing import Annotated
+from fastapi import APIRouter, Depends
 
 router = APIRouter(
     prefix="/users",
@@ -7,6 +10,21 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+@router.get("/{user_id}", response_model=UserPublic)
+async def read(user_id: int, service: Annotated[UserService, Depends()]):
+    return service.read(User, user_id)
+
+
+@router.get("/", response_model=list[UserPublic])
+async def read_all(service: Annotated[UserService, Depends()]):
+    return service.read_all(User)
+
+
+@router.post("/", response_model=UserPublic)
+async def create(user: UserCreate, service: Annotated[UserService, Depends()]):
+    return service.create(user)
+
+
+@router.put("/", response_model=UserPublic)
+async def update(user: UserUpdate, service: Annotated[UserService, Depends()]):
+    return service.update(user)
