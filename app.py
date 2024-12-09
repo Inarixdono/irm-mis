7d2bde015f2e1e2fc914bf93bf03ba3da8ef6770
IRM IT <itramosmotors@gmail.com>
2024-12-09 17:56:53 -0400

    feat: add Role router to main application for role management

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from core.database import init_db
 from service.person import controller as PersonController
 from service.branch import controller as BranchController
 from service.user import controller as UserController
+from service.user.role import router as role_router
 
 app = FastAPI(lifespan=init_db)
 
 app.include_router(PersonController.router)
 app.include_router(BranchController.router)
 app.include_router(UserController.router)
+app.include_router(role_router)
 
 
 @app.get("/")
