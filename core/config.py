9faa24c990d98a31d2e59f6fbd8ce79b49530bf0
IRM IT <itramosmotors@gmail.com>
2024-12-09 12:28:12 -0400

    feat: add password hashing context to configuration

--- core/config.py
+++ core/config.py
@@ -1,8 +1,11 @@
 import os
+from passlib.context import CryptContext
 
 
 class Config:
     DATABASE_DEVELOPMENT = os.getenv("DATABASE_DEVELOPMENT")
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 
 settings = Config()
