cab9a23a40b01ea6c15817dc69c5ca9ae9f746a4
IRM IT <itramosmotors@gmail.com>
2024-12-10 11:12:26 -0400

    feat: add UserDepartmentLink model for linking users to departments

--- service/user/link.py
+++ service/user/link.py
@@ -6,3 +6,10 @@ class UserRoleLink(SQLModel, table=True):
 
     user_id: int = Field(foreign_key="user.id", primary_key=True)
     role_id: int = Field(foreign_key="role.id", primary_key=True)
+
+
+class UserDepartmentLink(SQLModel, table=True):
+    __tablename__ = "user_department"
+
+    user_id: int = Field(foreign_key="user.id", primary_key=True, unique=True)
+    department_id: int = Field(foreign_key="department.id", primary_key=True)
