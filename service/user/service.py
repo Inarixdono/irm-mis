d43f3012ef506e90b29266b21447f48887558715
IRM IT <itramosmotors@gmail.com>
2024-12-10 16:37:51 -0400

    feat: enhance user creation and update methods to include roles and department handling

--- service/user/service.py
+++ service/user/service.py
@@ -1,16 +1,34 @@
+from .department import Department
+from .model import UserCreate, UserUpdate, User as UserModel
+from .role import Role
+
+# from .role import Role
 from core.crud import CRUD
 from core.config import settings
-from .model import UserCreate, UserUpdate, User as UserModel
 
 
 class User(CRUD):
-    def create(self, user: UserCreate):
-        user.password = self.__hash_password(user.password)
+    def create(self, user_create: UserCreate, roles: list[int], department_id: int):
+        user_create.password = self.__hash_password(user_create.password)
+        user = UserModel(
+            **user_create.model_dump(),
+            roles=self._get_roles(roles),
+            department=self._get_department(department_id),
+        )
         return super().create(UserModel, user)
-    
-    def update(self, user: UserUpdate):
-        user.password = self.__hash_password(user.password)
-        return super().update(UserModel, user)
+
+    def update(self, user_update: UserUpdate):
+        user_update.password = self.__hash_password(user_update.password)
+        return super().update(UserModel, user_update)
+
+    def __get_role(self, role_id: int):
+        return super().read(Role, role_id)
+
+    def _get_roles(self, roles: list[int]):
+        return list(map(self.__get_role, roles))
+
+    def _get_department(self, department_id: int):
+        return super().read(Department, department_id) if department_id else None
 
     def __hash_password(self, password: str):
         return settings.pwd_context.hash(password)
