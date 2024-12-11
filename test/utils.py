c2911628497d58d1b24ba5ca870ca747e375c694
IRM IT <itramosmotors@gmail.com>
2024-12-11 16:01:35 -0400

    feat: update branch_example data and add person_example with relevant fields

--- test/utils.py
+++ test/utils.py
@@ -1,4 +1,10 @@
 branch_example = {
-    "name": "TEST",
-    "phone_number": "000000000",
+    "name": "SHINJUKU",
+    "phone_number": "0000000000",
+}
+
+person_example = {
+    "name": "SATORU GOJO",
+    "document_type": "national_id",
+    "document_number": "40255632169",
 }
