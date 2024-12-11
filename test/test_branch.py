3c01e2fbc7529961266c4591f31c7521e138c1e6
IRM IT <itramosmotors@gmail.com>
2024-12-11 17:31:41 -0400

    refactor: streamline branch read and update test assertions

--- test/test_branch.py
+++ test/test_branch.py
@@ -10,8 +10,7 @@ def test_create_branch(client: TestClient):
 def test_read_branch(client: TestClient):
     response = client.get("/branches/1")
     assert response.status_code == 200
-    data = response.json()
-    assert data["name"] == branch_example["name"]
+    assert response.json()["name"] == branch_example["name"]
 
 
 def test_read_all_branches(client: TestClient):
@@ -25,5 +24,4 @@ def test_update_branch(client: TestClient):
     branch_example["name"] = "SHINJUKU"
     response = client.put("/branches/", json=branch_example)
     assert response.status_code == 200
-    content = response.json()
-    assert content["name"] == branch_example["name"]
+    assert response.json()["name"] == branch_example["name"]
