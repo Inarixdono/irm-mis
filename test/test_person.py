12053ee4396b7b10a6e62216d18434e98896b598
IRM IT <itramosmotors@gmail.com>
2024-12-11 16:01:46 -0400

    feat: add CRUD tests for person resource using FastAPI test client

---
+++ test/test_person.py
@@ -0,0 +1,29 @@
+from fastapi.testclient import TestClient
+from .utils import person_example
+
+
+def test_create_person(client: TestClient):
+    response = client.post("/persons/", json=person_example)
+    assert response.status_code == 200
+
+
+def test_read_person(client: TestClient):
+    response = client.get("/persons/1")
+    assert response.status_code == 200
+    content = response.json()
+    assert content["name"] == person_example["name"]
+
+
+def test_read_all_persons(client: TestClient):
+    response = client.get("/persons/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+
+def test_update_person(client: TestClient):
+    person_example["id"] = 1
+    person_example["name"] = "SUGURU GETO"
+    response = client.put("/persons/", json=person_example)
+    assert response.status_code == 200
+    content = response.json()
+    assert content["name"] == person_example["name"]
