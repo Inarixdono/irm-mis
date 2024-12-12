495bcf8cd8bfac6a860cf9a6b0d2fdfb64f351b6
IRM IT <itramosmotors@gmail.com>
2024-12-12 16:44:26 -0400

    feat: add superuser configuration to settings and update compose file

--- core/config.py
+++ core/config.py
@@ -2,12 +2,17 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_development: str
-    database_production: str
-    database_test: str
-    secret_key: str
-    carolina_user: str
-    carolina_password: str
+    DATABASE_DEVELOPMENT: str
+    DATABASE_PRODUCTION: str
+    DATABASE_TEST: str
+    SECRET_KEY: str
+    CAROLINA_USER: str
+    CAROLINA_PASSWORD: str
+    SUPERUSER_EMAIL: str
+    SUPERUSER_PASSWORD: str
+    SUPERUSER_NAME: str
+    SUPERUSER_DOCUMENT_NUMBER: str
+    
 
 
 settings = Settings()
