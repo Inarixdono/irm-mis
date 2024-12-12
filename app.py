98e7abb72f1561d6a91b3110b736bcf64ef717d3
IRM IT <itramosmotors@gmail.com>
2024-12-12 15:11:25 -0400

    feat: add exception handler for IntegrityError to improve error management

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 from core.database import init_db
 from api import api_router
 
+from sqlalchemy.exc import IntegrityError
+
+from core.error_handler import integrity_error_handler
+
 app = FastAPI(lifespan=init_db)
 
 app.include_router(api_router)
+
+app.add_exception_handler(IntegrityError, integrity_error_handler)
 
 
 @app.get("/")
