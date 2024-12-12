36914f22b9c27dcc67b096273cf69fe4243a4847
IRM IT <itramosmotors@gmail.com>
2024-12-12 10:40:32 -0400

    refactor: update test fixtures to use session scope and integrate CRUD operations

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,16 @@
 from core.database import engine
+from core.crud import CRUD
 from app import app
 from collections.abc import Generator
 import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import SQLModel, Session
+
+
+@pytest.fixture(scope="module")
+def client() -> Generator[TestClient, None, None]:
+    with TestClient(app) as c:
+        yield c
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -13,7 +20,8 @@ def db() -> Generator[Session, None, None]:
         SQLModel.metadata.drop_all(engine)
 
 
-@pytest.fixture(scope="module")
-def client() -> Generator[TestClient, None, None]:
-    with TestClient(app) as c:
-        yield c
+@pytest.fixture(scope="session", autouse=True)
+def crud(db: Session) -> Generator[CRUD, None, None]:
+    SQLModel.metadata.create_all(engine)
+    with db as session:
+        yield CRUD(session)
