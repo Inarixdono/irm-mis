1028a9bf4cc4f6dd4ecc46ec4260e76dfebd6205
IRM IT <itramosmotors@gmail.com>
2024-12-13 11:58:05 -0400

    feat: implement user creation logic for superuser during database initialization

--- core/database.py
+++ core/database.py
@@ -1,15 +1,51 @@
+from core.config import settings
+from service.user.model import User
 from contextlib import asynccontextmanager
 from typing import Annotated
-from core.config import settings
-from fastapi import Depends, FastAPI
-from sqlmodel import Session, create_engine, SQLModel
+from fastapi import FastAPI, Depends
+from sqlmodel import SQLModel, Session, create_engine, select
 
 engine = create_engine(settings.DATABASE_DEVELOPMENT)
+
+
+def create_first_user(session: Session):
+    from core.types import DocumentType
+    from service.person.model import Person
+    from service.branch.model import Branch
+    from service.user.department import Department
+    from service.user.role import Role
+
+    user = User(
+        info=Person(
+            name=settings.SUPERUSER_NAME,
+            document_type=DocumentType.national_id,
+            document_number=settings.SUPERUSER_DOCUMENT_NUMBER,
+        ),
+        email=settings.SUPERUSER_EMAIL,
+        password=settings.SUPERUSER_PASSWORD,
+        branch=Branch(name="Puerto Plata", phone_number="809-586-0100"),
+        department=Department(name="Development"),
+        roles=[Role(name="Superuser")],
+    )
+
+    session.add(user)
+    session.commit()
+    return session.refresh(user)
+
+
+def init():
+    with Session(engine) as session:
+        user = session.exec(
+            select(User).where(User.email == settings.SUPERUSER_EMAIL)
+        ).first()
+        if not user:
+            create_first_user(session)
 
 
 @asynccontextmanager
 async def init_db(app: FastAPI):
     SQLModel.metadata.create_all(engine)
+    init()
     yield
 
 
