bb5f218025f2dc9df22e8f70214b65a26413f1f3
IRM IT <itramosmotors@gmail.com>
2024-12-13 15:55:32 -0400

    refactor: rename and reorder branch test functions for clarity

--- test/api/test_branch.py
+++ test/api/test_branch.py
@@ -1,14 +1,16 @@
 from ..helper import branch_example, create_branch
+from core.config import settings
 from fastapi.testclient import TestClient
+
+
+def test_first_branch(client: TestClient):
+    response = client.get("/branches/1")
+    assert response.status_code == 200
+    assert response.json()["name"] == settings.SUPERUSER_BRANCH
 
 
 def test_create_branch(client: TestClient):
     response = create_branch(client)
-    assert response.status_code == 200
-
-
-def test_read_branch(client: TestClient):
-    response = client.get("/branches/1")
     assert response.status_code == 200
     assert response.json()["name"] == branch_example["name"]
 
@@ -16,11 +18,11 @@ def test_read_branch(client: TestClient):
 def test_read_all_branches(client: TestClient):
     response = client.get("/branches/")
     assert response.status_code == 200
-    assert len(response.json()) == 1
+    assert len(response.json()) == 2
 
 
 def test_update_branch(client: TestClient):
-    branch_example["id"] = 1
+    branch_example["id"] = 2
     branch_example["name"] = "SHINJUKU"
     response = client.put("/branches/", json=branch_example)
     assert response.status_code == 200
