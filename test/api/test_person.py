d32fdfeb6d9283300d58903055b15451d06fe47c
IRM IT <itramosmotors@gmail.com>
2024-12-13 16:47:50 -0400

    test: update person tests for improved functionality and assertions

--- test/api/test_person.py
+++ test/api/test_person.py
@@ -1,5 +1,12 @@
-from fastapi.testclient import TestClient
 from ..helper import person_example, create_person
+from core.config import settings
+from fastapi.testclient import TestClient
+
+
+def test_first_person(client: TestClient):
+    response = client.get("/persons/1")
+    assert response.status_code == 200
+    assert response.json()["name"] == settings.SUPERUSER_NAME
 
 
 def test_create_person(client: TestClient):
@@ -7,20 +14,14 @@ def test_create_person(client: TestClient):
     assert response.status_code == 200
 
 
-def test_read_person(client: TestClient):
-    response = client.get("/persons/1")
-    assert response.status_code == 200
-    assert response.json()["name"] == person_example["name"]
-
-
 def test_read_all_persons(client: TestClient):
     response = client.get("/persons/")
     assert response.status_code == 200
-    assert len(response.json()) == 1
+    assert len(response.json()) == 2
 
 
 def test_update_person(client: TestClient):
-    person_example["id"] = 1
+    person_example["id"] = 2
     person_example["name"] = "SUGURU GETO"
     response = client.put("/persons/", json=person_example)
     assert response.status_code == 200
