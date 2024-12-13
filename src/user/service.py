338785e5e43759b46c19039202571ab083b382d4
IRM IT <itramosmotors@gmail.com>
2024-12-13 12:03:55 -0400

    refactor: remove unnecessary session initialization in User service

--- src/user/service.py
+++ src/user/service.py
@@ -1,14 +1,11 @@
 from .department import Department
 from .model import UserCreate, UserUpdate, User as UserModel
 from .role import Role
-from core.crud import CRUD, SessionDependency
+from core.crud import CRUD
 from core.security import get_password_hash
 
 
 class User(CRUD):
-    def __init__(self, session: SessionDependency):
-        super().__init__(session)
-
     def create(self, user_create: UserCreate, roles: list[int], department_id: int):
         extra_data = {
             "password": get_password_hash(user_create.password),
