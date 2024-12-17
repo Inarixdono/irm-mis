e4c8e812cfb2d19a614d920799a5c4c3fa7bed7e
IRM IT <itramosmotors@gmail.com>
2024-12-17 16:16:51 -0400

    feat: include authentication router in API router

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ from src.branch.controller import router as branch_router
 from src.user.controller import router as user_router
 from src.user.role import router as role_router
 from src.user.department import router as department_router
+from src.auth.controller import router as auth_router
 
 api_router = APIRouter()
 
@@ -13,3 +14,4 @@ api_router.include_router(branch_router)
 api_router.include_router(user_router)
 api_router.include_router(role_router)
 api_router.include_router(department_router)
+api_router.include_router(auth_router)
