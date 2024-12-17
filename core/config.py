afd5e3994b6ab707e502cea2a75431c65f1881e8
IRM IT <itramosmotors@gmail.com>
2024-12-17 16:07:08 -0400

    feat: add secret key and token configuration to settings for enhanced security

--- core/config.py
+++ core/config.py
@@ -5,9 +5,12 @@ class Settings(BaseSettings):
     DATABASE_DEVELOPMENT: str
     DATABASE_PRODUCTION: str
     DATABASE_TEST: str
-    SECRET_KEY: str
     CAROLINA_USER: str
     CAROLINA_PASSWORD: str
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 180
 
     SUPERUSER_EMAIL: str
     SUPERUSER_PASSWORD: str
