913d0838a9da2710b16673210b5e39f7da826de2
IRM IT <itramosmotors@gmail.com>
2024-12-18 15:34:39 -0400

    feat: update person API tests to include authentication headers

--- test/api/test_person.py
+++ test/api/test_person.py
@@ -1,28 +1,30 @@
-from ..helper import person_example, create_person
 from core.config import settings
 from fastapi.testclient import TestClient
 
+from ..helper import person_example
 
-def test_first_person(client: TestClient):
-    response = client.get("/persons/1")
+
+def test_first_person(client: TestClient, headers: dict):
+    response = client.get("/persons/1", headers=headers)
     assert response.status_code == 200
     assert response.json()["name"] == settings.SUPERUSER_NAME
 
 
-def test_create_person(client: TestClient):
-    response = create_person(client)
+def test_create_person(client: TestClient, headers: dict):
+    response = client.post("/persons/", json=person_example, headers=headers)
     assert response.status_code == 200
 
 
-def test_read_all_persons(client: TestClient):
-    response = client.get("/persons/")
+def test_read_all_persons(client: TestClient, headers: dict):
+    response = client.get("/persons/", headers=headers)
     assert response.status_code == 200
     assert len(response.json()) == 2
 
 
-def test_update_person(client: TestClient):
+def test_update_person(client: TestClient, headers: dict):
+    person_before = person_example.copy()
     person_example["id"] = 2
     person_example["name"] = "SUGURU GETO"
-    response = client.put("/persons/", json=person_example)
+    response = client.put("/persons/", json=person_example, headers=headers)
     assert response.status_code == 200
-    assert response.json()["name"] == person_example["name"]
+    assert response.json()["name"] != person_before["name"]
