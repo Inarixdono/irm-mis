e4138a37c0bf7a674d27fccb7a6258d33e3223ac
IRM IT <itramosmotors@gmail.com>
2024-12-18 15:47:38 -0400

    feat: update user API tests to include authentication headers

--- test/api/test_user.py
+++ test/api/test_user.py
@@ -1,29 +1,36 @@
-from ..helper import user_example, create_user
 from core.config import settings
 from fastapi.testclient import TestClient
 
+from ..helper import person_example, user_example
 
-def test_first_user(client: TestClient):
-    response = client.get("/users/1")
+
+def test_first_user(client: TestClient, headers: dict):
+    response = client.get("/users/1", headers=headers)
     assert response.status_code == 200
     assert response.json()["email"] == settings.SUPERUSER_EMAIL
 
 
-def test_create_user(client: TestClient):
-    response = create_user(client)
+def test_create_user(client: TestClient, headers: dict):
+    data = {
+        "info": person_example,
+        "user": user_example,
+        "roles": [1],
+    }
+    response = client.post("/users/", json=data, headers=headers)
     assert response.status_code == 200
     assert response.json()["email"] == user_example["email"]
 
 
-def test_read_all_users(client: TestClient):
-    response = client.get("/users/")
+def test_read_all_users(client: TestClient, headers: dict):
+    response = client.get("/users/", headers=headers)
     assert response.status_code == 200
     assert len(response.json()) == 2
 
 
-def test_update_user(client: TestClient):
+def test_update_user(client: TestClient, headers: dict):
+    user_before = user_example.copy()
     user_example["id"] = 2
     user_example["email"] = "getosuguru@spiritmanipulation.com"
-    response = client.put("/users/", json=user_example)
+    response = client.put("/users/", json=user_example, headers=headers)
     assert response.status_code == 200
-    assert response.json()["email"] == user_example["email"]
+    assert response.json()["email"] != user_before["email"]
