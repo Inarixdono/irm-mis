d6351abde584ae7a6d16c23c85e5472ce694d9b8
IRM IT <itramosmotors@gmail.com>
2024-12-18 09:31:27 -0400

    feat: enhance person model tests with additional assertions and update logic

--- test/models/test_person.py
+++ test/models/test_person.py
@@ -1,20 +1,47 @@
 from ..helper import person_example
-from src.person.model import Person, PersonCreate, PersonUpdate
 from core.crud import CRUD
+from core.config import settings
+from src.person.model import Person, PersonCreate, PersonUpdate
+
+
+def test_first_person(crud: CRUD):
+    person: Person = crud.read(Person, 1)
+    assert person.name == settings.SUPERUSER_NAME
+    assert person.document_number == settings.SUPERUSER_DOCUMENT_NUMBER
+    assert person.is_active
+    assert person.created_at is not None
+    assert person.updated_at is None
 
 
 def test_create_person(crud: CRUD):
     person_create = PersonCreate(**person_example)
     person: Person = crud.create(Person, person_create)
     assert hasattr(person, "id")
-    assert person.name == person_create.name
+    assert person.name == person_example["name"]
+    assert person.document_number == person_example["document_number"]
+    assert person.is_active
     assert person.created_at is not None
     assert person.updated_at is None
 
+
 def test_update_person(crud: CRUD):
-    person_example["id"] = 1
-    person_example["name"] = "SUGURU GETO"
-    person_update = PersonUpdate(**person_example)
-    person: Person = crud.update(Person, person_update)
-    assert person.name == person_update.name
-    assert person.updated_at is not None
+    person_to_update: Person = crud.read(Person, 2)
+    person_to_update.name = "SUGURU GETO"
+    person_to_update.document_number = "45856988521"
+    updated_person: Person = crud.update(
+        Person,
+        PersonUpdate(
+            id=person_to_update.id,
+            name=person_to_update.name,
+            document_number=person_to_update.document_number,
+        ),
+    )
+    assert updated_person.name == person_to_update.name
+    assert updated_person.document_number == person_to_update.document_number
+    assert updated_person.created_at == person_to_update.created_at
+    assert updated_person.updated_at is not None
+
+
+def test_total_persons(crud: CRUD):
+    total_persons = crud.read_all(Person)
+    assert len(total_persons) == 2
