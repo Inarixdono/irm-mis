92066c043109170e844153a7aff0ed16dc290bb5
IRM IT <itramosmotors@gmail.com>
2024-12-18 12:54:38 -0400

    feat: enhance CRUD update method to accept extra data and improve resource validation

--- core/crud.py
+++ core/crud.py
@@ -20,15 +20,17 @@ class CRUD:
         return self.session.exec(statement).all()
 
     def create(self, base_model: SQLModel, model_create: SQLModel, extra_data=None):
+        print(model_create)
         resource = base_model.model_validate(model_create, update=extra_data)
         return self.__commit(resource)
 
-    def update(self, base_model: SQLModel, model_update: ModelUpdate):
-        extra_data = {}
+    def update(
+        self, base_model: SQLModel, model_update: ModelUpdate, extra_data: dict = {}
+    ):
         resource: SQLModel = self.session.get(base_model, model_update.id)
 
         if issubclass(base_model, Audit):
-            extra_data = {"updated_at": datetime.now()}
+            extra_data.update({"updated_at": datetime.now()})
 
         resource_data = model_update.model_dump(exclude_unset=True)
         resource.sqlmodel_update(resource_data, update=extra_data)
