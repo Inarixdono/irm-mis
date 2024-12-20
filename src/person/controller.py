5bd1b522fe8260321e7e276522c0467fad854fea
IRM IT <itramosmotors@gmail.com>
2024-12-20 16:26:55 -0400

    refactor: update person controller to use instance of PersonService and simplify method calls

--- src/person/controller.py
+++ src/person/controller.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from src.person.model import PersonCreate, PersonUpdate, PersonPublic, Person
+from src.person.model import PersonCreate, PersonUpdate, PersonPublic
 from src.person.service import Person as PersonService
 from fastapi import APIRouter, Depends
 
@@ -9,28 +9,31 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+person_service = PersonService()
+
 
 @router.get("/{person_id}", response_model=PersonPublic)
-async def read_person(person_id: int, service: Annotated[PersonService, Depends()]):
-    return service.read(Person, person_id)
+async def read_person(
+    person_id: int, service: Annotated[PersonService, Depends(person_service)]
+):
+    return service.read(person_id)
 
 
 @router.get("/", response_model=list[PersonPublic])
-async def read_all(service: Annotated[PersonService, Depends()]):
-    return service.read_all(Person)
+async def read_all(service: Annotated[PersonService, Depends(person_service)]):
+    return service.read_all()
 
 
 @router.post("/", response_model=PersonPublic)
 async def create_person(
-    person: PersonCreate, service: Annotated[PersonService, Depends()]
+    person: PersonCreate, service: Annotated[PersonService, Depends(person_service)]
 ):
-    return service.create(Person, person)
-
+    return service.create(person)
 
 
 @router.put("/", response_model=PersonPublic)
 async def update_person(
     person: PersonUpdate,
-    service: Annotated[PersonService, Depends()],
+    service: Annotated[PersonService, Depends(person_service)],
 ):
-    return service.update(Person, person)
+    return service.update(person)
--- src/person/service.py
+++ src/person/service.py
@@ -1,5 +1,7 @@
+from .model import Person as PersonModel
 from core.crud import CRUD
 
 
 class Person(CRUD):
-    pass
+    def __init__(self):
+        super().__init__(PersonModel)
