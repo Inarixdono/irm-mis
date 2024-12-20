48e248b29121a5cdaa29cc3648e4c81048335b20
IRM IT <itramosmotors@gmail.com>
2024-12-20 11:18:24 -0400

    refactor: remove unnecessary dependency on current user in API router

--- api.py
+++ api.py
@@ -1,12 +1,11 @@
-from core.security import get_current_user
 from src.person.controller import router as person_router
 from src.branch.controller import router as branch_router
 from src.user.controller import router as user_router
 from src.user.role import router as role_router
 from src.user.department import router as department_router
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 
-api_router = APIRouter(dependencies=[Depends(get_current_user)])
+api_router = APIRouter()
 
 api_router.include_router(person_router)
 api_router.include_router(branch_router)
