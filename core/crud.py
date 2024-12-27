c14bc579f848307fb5612ab34cb136c052d002f8
IRM IT <itramosmotors@gmail.com>
2024-12-27 08:25:10 -0400

    refactor: update CRUD methods to return TableModel for improved consistency

--- core/crud.py
+++ core/crud.py
@@ -1,6 +1,6 @@
-from core.security import get_current_user
-from core.types import  Auditable, UpdateModel
 from core.database import SessionDependency, SQLModel
+from core.security import get_current_user
+from core.types import  Auditable, TableModel, UpdateModel
 from src.auth.model import TokenData
 from typing import Annotated
 from datetime import datetime
@@ -24,19 +24,19 @@ class CRUD:
         self.current_user = current_user
         return self
 
-    def read(self, id: int) -> SQLModel:
+    def read(self, id: int) -> TableModel:
         return self.__read_active(id)
 
-    def read_all(self) -> list[SQLModel]:
+    def read_all(self) -> list[TableModel]:
         return self.session.exec(
             select(self.base_model).where(self.base_model.is_active)
         ).all()
 
-    def create(self, model_create: SQLModel, extra_data: dict = {}) -> SQLModel:
+    def create(self, model_create: SQLModel, extra_data: dict = {}) -> TableModel:
         resource = self.__validate_model(model_create, extra_data)
         return self.__commit(resource)
 
-    def update(self, model_update: UpdateModel, update_data: dict = {}) -> SQLModel:
+    def update(self, model_update: UpdateModel, update_data: dict = {}) -> TableModel:
         resource = self.read(model_update.id)
 
         update_data.update(
@@ -47,12 +47,12 @@ class CRUD:
         resource.sqlmodel_update(resource_data, update=update_data)
         return self.__commit(resource)
 
-    def delete(self, id: int) -> SQLModel:
+    def delete(self, id: int) -> TableModel:
         resource = self.read(id)
         self.update(UpdateModel(id=id), {"is_active": False})
         return resource
 
-    def __read_active(self, id: int) -> SQLModel:
+    def __read_active(self, id: int) -> TableModel:
         resource = self.session.exec(
             select(self.base_model).where(
                 self.base_model.id == id, self.base_model.is_active
@@ -64,11 +64,11 @@ class CRUD:
 
         return resource
 
-    def __validate_model(self, incoming_data: SQLModel, extra_data: dict) -> SQLModel:
+    def __validate_model(self, incoming_data: SQLModel, extra_data: dict) -> TableModel:
         extra_data.update({"created_by": self.current_user.id})
         return self.base_model.model_validate(incoming_data, update=extra_data)
 
-    def __commit(self, resource) -> SQLModel:
+    def __commit(self, resource) -> TableModel:
         self.session.add(resource)
         self.session.commit()
         self.session.refresh(resource)
