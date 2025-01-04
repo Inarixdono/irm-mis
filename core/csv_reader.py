af6753d43ced31af9a4132dad3d2b2d4e37ca25b
IRM IT <itramosmotors@gmail.com>
2025-01-04 10:11:13 -0400

    feat: update get_content method in CSVReader to specify return type

--- core/csv_reader.py
+++ core/csv_reader.py
@@ -1,8 +1,9 @@
+from typing import Any
 from pandas import read_csv
 from fastapi import UploadFile
 
 
 class CSVReader:
-    def get_content(self, file: UploadFile):
+    def get_content(self, file: UploadFile) -> list[dict[str, Any]]:
         content = read_csv(file, delimiter=";")
         return content.to_dict(orient="records")
