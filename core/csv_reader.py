833251e6fdc00ce496a01666261ece515677e17c
IRM IT <itramosmotors@gmail.com>
2025-01-07 10:16:41 -0400

    feat: update get_content method in CSVReader to return a generator for improved memory efficiency

--- core/csv_reader.py
+++ core/csv_reader.py
@@ -1,9 +1,11 @@
+from typing import Generator
 from typing import Any
 from pandas import read_csv
 from fastapi import UploadFile
 
 
 class CSVReader:
-    def get_content(self, file: UploadFile) -> list[dict[str, Any]]:
-        content = read_csv(file, delimiter=";")
-        return content.to_dict(orient="records")
+    def get_content(self, file: UploadFile) -> Generator[Any, None, None]:
+        df = read_csv(file, delimiter=";")
+        for row in df.itertuples(index=False):
+            yield row
