2cbed2a99f2bc3f54f27182040c8f0056658d569
IRM IT <itramosmotors@gmail.com>
2025-01-07 10:20:28 -0400

    feat: update get_content method in CSVReader to return an Iterable for improved type clarity

--- core/csv_reader.py
+++ core/csv_reader.py
@@ -1,11 +1,11 @@
-from typing import Generator
+from typing import Iterable
 from typing import Any
 from pandas import read_csv
 from fastapi import UploadFile
 
 
 class CSVReader:
-    def get_content(self, file: UploadFile) -> Generator[Any, None, None]:
+    def get_content(self, file: UploadFile) -> Iterable[Any]:
         df = read_csv(file, delimiter=";")
         for row in df.itertuples(index=False):
             yield row
