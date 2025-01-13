cf56b6cbe45c92cad9f289eb29a70ed43bdbc7ce
IRM IT <itramosmotors@gmail.com>
2025-01-13 17:27:35 -0400

    refactor: streamline security module by integrating JWTManager for token decoding

--- core/security.py
+++ core/security.py
@@ -1,60 +1,19 @@
-import jwt
-from datetime import datetime, timedelta, timezone
-from core.config import settings
-from core.exceptions import (
-    InsufficientPermissionsException,
-    InvalidCredentialsException,
-)
+from core.exceptions import InsufficientPermissionsException
 from core.types import Role
+from security import JWTManager
 from src.auth import TokenData
-from src.user import User
 from typing import Annotated
-from passlib.context import CryptContext
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-
-
-def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password: str):
-    return pwd_context.hash(password)
-
-
-def create_access_token(user: User, expires: timedelta | None = None):
-    if expires:
-        expire = datetime.now(timezone.utc) + expires
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    return jwt.encode(
-        payload={
-            "id": user.id,
-            "name": user.name,
-            "email": user.email,
-            "role": user.role,
-            "department": user.department,
-            "branch_id": user.branch_id,
-            "exp": expire,
-        },
-        key=settings.SECRET_KEY,
-        algorithm=settings.ALGORITHM,
-    )
 
 
 def get_current_user(
     token: Annotated[str, Depends(oauth2_scheme)],
 ) -> TokenData:
-    try:
-        payload = jwt.decode(
-            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
-        )
-    except jwt.InvalidTokenError:
-        raise InvalidCredentialsException()
+    payload = JWTManager.decode(token)
     return TokenData(
         id=payload.get("id"),
         name=payload.get("name"),
@@ -75,4 +34,3 @@ def is_admin(user: Annotated[TokenData, Depends(get_current_user)]):
     if user.role not in [Role.SUPERUSER, Role.ADMIN]:
         raise InsufficientPermissionsException()
     return True
-
